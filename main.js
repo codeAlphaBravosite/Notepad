1a28078dd7b794e0c12c93741897158655444b90
codeAlphaBravosite <help.factchannel@gmail.com>
2024-11-13 14:52:27 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import { NoteManager } from './noteManager.js';
-import { UIManager } from './uiManager.js';
-
-const noteManager = new NoteManager();
-const uiManager = new UIManager(noteManager);
-
-uiManager.initialize();
